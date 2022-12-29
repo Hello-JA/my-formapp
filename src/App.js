ba57f514daf0ef2714fe3c3b97e21ce5ac2bc00d
Jose Angel <kikomalbranche@gmail.com>
2022-12-29 20:11:59 -0300

    my-formapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    lastname: '',
+    description: '',
+    country: '',
+    agree: '',
+    desagree: '',
+  })
+
+  const onChange = (e) => {
+    const { value, name, type, checked } = e.target
+
+    setForm((state) => ({
+      ...state,
+      [name]: type === 'checkbox' ? checked : value,
+    }))
+  }
+
+  const showData = () => {
+    console.log('Form: ', form)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Registration Form</h3>
+        <form onSubmit={onSubmit}>
+          <label>
+            Name:
+            <input onChange={onChange} name="name" value={form.name}  required/>
+          </label>
+          <hr />
+          <label>
+            Last Name:
+            <input onChange={onChange} name="lastname" value={form.lastname} required/>
+          </label>
+          <hr />
+          <label>
+            Email:
+            <input onChange={onChange} name="email" value={form.email} required/>
+          </label>
+          <hr />
+          <label>
+            Description:
+            <textarea
+              onChange={onChange}
+              name="description"
+              value={form.description}
+            />
+          </label>
+          <hr />
+          <label>
+            Country:
+            <select onChange={onChange} name="country">
+              <option value="Other">Other</option>
+              <option value="BR">Brasil</option>
+              <option value="CB">Cuba</option>
+              <option value="VN">Venezuela</option>
+            </select>
+          </label>
+          <h5> Are you agree or desagree with the service?</h5>
+          <label>
+            agree:
+            <input
+              type="checkbox"
+              onChange={onChange}
+              name="agree"
+              value={form.agree}
+            />
+            desagree:
+            <input
+              type="checkbox"
+              onChange={onChange}
+              name="desagree"
+              value={form.desagree}
+             />
+          </label>
+
+          <div>
+            <button onClick={showData}>Submit</button>
+          </div>
+        </form>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
